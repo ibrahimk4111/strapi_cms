26cbf69413cc1736f1c910a429ae0c4c4d931522
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-06-28 11:45:20 +0600

    postgres added

--- config/server.js
+++ config/server.js
@@ -1,21 +1,10 @@
-module.exports = ({ env }) => {
-  
-  const host = env('HOST', '0.0.0.0');
-  const port = process.env.PORT || 4000;
-  console.log(`Strapi is binding to host ${host} on port ${port}`);
-
-  return {
-  host,
-  port,
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'vd6zi0IlWEWTY7Ri9vkSpQ=='),
-    },
-  },
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-}};
+});
