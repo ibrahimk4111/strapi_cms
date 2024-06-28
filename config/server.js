1beeb8fadeada1b70103a8a7b70147677c3b51b6
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-06-28 12:18:56 +0600

    port changed

--- config/server.js
+++ config/server.js
@@ -1,10 +1,16 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337) || 4000,
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-});
+// config/server.js
+module.exports = ({ env }) => {
+  const host = env('HOST', '0.0.0.0');
+  const port = env.int('PORT', process.env.PORT || 1337);
+  console.log(`Strapi is binding to host ${host} on port ${port}`);
+  return {
+    host,
+    port,
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+    webhooks: {
+      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    },
+  };
+};
