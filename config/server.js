5f75d9372a982e59a6fa7dc3a3693e3b716650e1
ibrahimk4111 <ibrahimk4111@gmail.com>
2024-06-28 08:56:34 +0600

    server.js setting changed

--- config/server.js
+++ config/server.js
@@ -1,6 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a56s4df#@1a6sd5f1a65sd1f#@6a5sd1f65a1sdf65a1'),
+    },
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
